7d4c20be1a177f83e16d0b84905d09893d235167
djemoui <34734830+badrdjemoui@users.noreply.github.com>
2025-10-16 16:17:04 +0300

    Update Program.cs

--- v1-classes/Program.cs
+++ v1-classes/Program.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Windows.Forms;
+
+namespace AnimalFeedApp
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
 
